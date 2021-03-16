5596fff4d0fc37fd781a46b64f2fdf9d796c7f20
mrHeavenli <71031776+mrHeavenli@users.noreply.github.com>
2021-03-16 14:48:28 +0100

    Update 2darray.py

--- 2darray.py
+++ 2darray.py
@@ -12,3 +12,18 @@ def init_arr2d_square(x):
         arr2d.append(z)  # Actual 2D, z is appended to main list
 
     return arr2d
+
+def init_arr2d_rect(z, x):
+
+    # Same as before, just that you can choose the lengths
+
+    arr2d = [] # Main array(actually list)
+    zaxis = []  # Z axis
+
+    for i in range(1, z):
+        z.append("") # Adding empty strings to z to define length
+
+    for i in range(1, x):
+        arr2d.append(zaxis) 
+
+    return arr2d
