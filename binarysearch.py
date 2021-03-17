48c9270612aa27204a225b7a6bde3732ff06f5ab
mrHeavenli <71031776+mrHeavenli@users.noreply.github.com>
2021-03-17 16:38:06 +0100

    Cleaned up code
    Added some spaces, that's all :)

--- binarysearch.py
+++ binarysearch.py
@@ -1,13 +1,25 @@
 def binary_search(arr: list, query: int) -> bool:
+    
+    
     middle = len(arr) // 2
+    
     if len(arr) == 1 and arr[middle] != query:
         return False
+    
+    
+    
     if arr[middle] < query:
-        return binary_search(arr[middle:], query)
+        return binary_search(arr[middle:], query) 
+    
     if arr[middle] > query:
         return binary_search(arr[:middle], query)
+    
+    
     if arr[middle] == query:
         return True
 
+    
+    
+    
 arr = [2, 4, 7, 12, 5434 , 2224345, 1423232543]
 print(binary_search(arr, 3))
